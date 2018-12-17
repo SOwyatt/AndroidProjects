f0a1e1ef00635397ead4f8202aedd30b5596df8a
School <SOwyatt@users.noreply.github.com>
2018-12-17 13:56:09 -0500

    12/17/2018 (0)

--- 1.2.xSysnntesizer/Synthesizer/app/src/main/java/com/example/travisbergeron20/synthesizer/SynthesizerActivity.java
+++ 1.2.xSysnntesizer/Synthesizer/app/src/main/java/com/example/travisbergeron20/synthesizer/SynthesizerActivity.java
@@ -1,7 +1,9 @@
 package com.example.travisbergeron20.synthesizer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.gsm.GsmCellLocation;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -10,21 +12,57 @@ public class SynthesizerActivity extends AppCompatActivity {
     private static final String TAG = SynthesizerActivity.class.getName();
     private Button button1;
     private Button button2;
+    private MediaPlayer mpA;
+    private MediaPlayer mpB;
+    private MediaPlayer mpBb;
+    private MediaPlayer mpC;
+    private MediaPlayer mpCs;
+    private MediaPlayer mpD;
+    private MediaPlayer mpDs;
+    private MediaPlayer mpE;
+    private MediaPlayer mpF;
+    private MediaPlayer mpFs;
+    private MediaPlayer mpG;
+    private MediaPlayer mpGs;
+    private MediaPlayer mphE;
+    private MediaPlayer mphF;
+    private MediaPlayer mphFs;
+    private MediaPlayer mphG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
 
-        button1 = (Button)findViewById(R.id.button1);
-        button2 = (Button)findViewById(R.id.button2);
+        button1 = (Button)findViewById(R.id.btnE);
+        button2 = (Button)findViewById(R.id.btnF);
+        mpA = MediaPlayer.create(this, R.raw.scalea);
+        mpB = MediaPlayer.create(this, R.raw.scaleb);
+        mpBb = MediaPlayer.create(this, R.raw.scalebb);
+        mpC = MediaPlayer.create(this, R.raw.scalec);
+        mpCs = MediaPlayer.create(this, R.raw.scalecs);
+        mpD = MediaPlayer.create(this, R.raw.scaled);
+        mpDs = MediaPlayer.create(this, R.raw.scaleds);
+        mpE = MediaPlayer.create(this, R.raw.scalee);
+        mpF = MediaPlayer.create(this, R.raw.scalef);
+        mpFs = MediaPlayer.create(this, R.raw.scalefs);
+        mpG = MediaPlayer.create(this, R.raw.scaleg);
+        mpGs = MediaPlayer.create(this, R.raw.scalegs);
+        mphE = MediaPlayer.create(this, R.raw.scalehighe);
+        mphF = MediaPlayer.create(this, R.raw.scalehighf);
+        mphFs = MediaPlayer.create(this, R.raw.scalehighfs);
+        mphG = MediaPlayer.create(this, R.raw.scalehighg);
     }
 
     public void onButton1Click(View v) {
-        Log.i(TAG, "Button 1 Clicked");
+        mpE.seekTo(0);
+        Log.i(TAG, "E Button Clicked");
+        mpE.start();
     }
 
     public void onButton2Click(View v) {
-        Log.i(TAG, "Button 2 Clicked");
+        mpF.seekTo(0);
+        Log.i(TAG, "F button clicked");
+        mpF.start();
     }
 }
