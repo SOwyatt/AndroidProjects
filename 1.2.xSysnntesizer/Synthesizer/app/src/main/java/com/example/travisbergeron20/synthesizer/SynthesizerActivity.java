e57b6610bb897b4fdbc2b06c6a6a86ba5a90f877
School <SOwyatt@users.noreply.github.com>
2019-01-18 13:30:28 -0500

    1/18/19

--- 1.2.xSysnntesizer/Synthesizer/app/src/main/java/com/example/travisbergeron20/synthesizer/SynthesizerActivity.java
+++ 1.2.xSysnntesizer/Synthesizer/app/src/main/java/com/example/travisbergeron20/synthesizer/SynthesizerActivity.java
@@ -14,8 +14,7 @@ public class SynthesizerActivity extends AppCompatActivity {
     private static final int WHOLE_NOTE = 1000;
 
     private Button challenge1_btn;
-    private NumberPicker numPickerDuration;
-    private NumberPicker numPickerNote;
+    private Button challenge2_btn;
 
     private MediaPlayer mpA;
     private MediaPlayer mpB;
@@ -34,6 +33,11 @@ public class SynthesizerActivity extends AppCompatActivity {
     private MediaPlayer mphFs;
     private MediaPlayer mphG;
 
+    //Challenge 2
+    private NumberPicker numPickerNote;
+    private NumberPicker numPickerMultiplicity;
+
+    //
     private MediaPlayer[] allNotes = new MediaPlayer[16];
 
     @Override
@@ -42,6 +46,9 @@ public class SynthesizerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_synthesizer);
 
         challenge1_btn = findViewById(R.id.btn_challenge1);
+        challenge2_btn = findViewById(R.id.btn_challenge2);
+
+
         allNotes[0] = mpA = MediaPlayer.create(this, R.raw.scalea);
         allNotes[1] = mpB = MediaPlayer.create(this, R.raw.scaleb);
         allNotes[2] = mpBb = MediaPlayer.create(this, R.raw.scalebb);
@@ -59,10 +66,14 @@ public class SynthesizerActivity extends AppCompatActivity {
         allNotes[14] = mphFs = MediaPlayer.create(this, R.raw.scalehighfs);
         allNotes[15] = mphG = MediaPlayer.create(this, R.raw.scalehighg);
 
-        numPickerDuration.setMaxValue(1000);
-        numPickerDuration.setMinValue(1);
-        numPickerNote.setMaxValue(15);
+        // Challenge 2
+        numPickerNote = (NumberPicker) findViewById(R.id.numPickerNote);
+        numPickerMultiplicity = (NumberPicker) findViewById(R.id.numPickerDuration);
+
         numPickerNote.setMinValue(0);
+        numPickerMultiplicity.setMinValue(0);
+        numPickerNote.setMaxValue(15);
+        numPickerMultiplicity.setMaxValue(5000);
     }
 
     private static void playNote(MediaPlayer note, int delay) {
@@ -108,7 +119,87 @@ public class SynthesizerActivity extends AppCompatActivity {
         playNote(mpE, WHOLE_NOTE/2);
     }
 
-    public void challenge2(View v) {
+    public void challenge2(View v) { // Also satisfies *challenge 4*
+        for(int i=0; i < numPickerMultiplicity.getValue(); ++i) {
+            playNote(allNotes[numPickerNote.getValue()], WHOLE_NOTE/4);
+        }
+    }
 
+    public void challenge3(View v) {
+        MediaPlayer[] scale = {allNotes[7], allNotes[9], allNotes[10],
+        allNotes[0], allNotes[1], allNotes[4], allNotes[5], allNotes[7]};
+        for(MediaPlayer s : scale) {
+            playNote(s, WHOLE_NOTE/2);
+        }
+    }
+
+    public void challenge5(View v) {
+        MediaPlayer[] twinkle = {mpA, mpA, mphE, mphE, mphFs, mphFs, mphE,
+        mpD, mpD, mpCs, mpCs, mpB, mpB, mpA};
+        for(MediaPlayer n : twinkle) {
+            playNote(n, WHOLE_NOTE/2);
+        }
+    }
+
+    //Challenge 6 helper
+    private void playTwinkle() {
+        MediaPlayer[] twinkle = {mpA, mpA, mphE, mphE, mphFs, mphFs, mphE,
+                mpD, mpD, mpCs, mpCs, mpB, mpB, mpA};
+        for(MediaPlayer n : twinkle) {
+            playNote(n, WHOLE_NOTE / 2);
+        }
+    }
+
+    public void challenge6(View v) { // Also completes challenge 7
+        playTwinkle();
+    }
+
+    public void challenge8(View v) {
+        playNote(mpA, WHOLE_NOTE/4);
+        playNote(mpA, WHOLE_NOTE/4);
+        playNote(mpE, WHOLE_NOTE/4);
+        playNote(mpE, WHOLE_NOTE/4);
+        playNote(mpFs, WHOLE_NOTE/4);
+        playNote(mpFs, WHOLE_NOTE/4);
+        playNote(mpE, WHOLE_NOTE/2);
+        playNote(mpD, WHOLE_NOTE/4);
+        playNote(mpCs, WHOLE_NOTE/2);
+        playNote(mpCs, WHOLE_NOTE/2);
+        playNote(mpB, WHOLE_NOTE/4);
+        playNote(mpB, WHOLE_NOTE/4);
+        playNote(mpA, WHOLE_NOTE/4);
+    }
+
+    private void line1() {
+        playNote(mpA, WHOLE_NOTE/4);
+        playNote(mpA, WHOLE_NOTE/4);
+        playNote(mpE, WHOLE_NOTE/4);
+        playNote(mpE, WHOLE_NOTE/4);
+        playNote(mpFs, WHOLE_NOTE/4);
+        playNote(mpFs, WHOLE_NOTE/4);
+        playNote(mpE, WHOLE_NOTE/2);
+        playNote(mpD, WHOLE_NOTE/4);
+        playNote(mpCs, WHOLE_NOTE/2);
+        playNote(mpCs, WHOLE_NOTE/2);
+        playNote(mpB, WHOLE_NOTE/4);
+        playNote(mpB, WHOLE_NOTE/4);
+        playNote(mpA, WHOLE_NOTE/4);
+    }
+
+    private void line2() {
+        playNote(mphE, WHOLE_NOTE/4);
+        playNote(mphE, WHOLE_NOTE/4);
+        playNote(mpD, WHOLE_NOTE/4);
+        playNote(mpD, WHOLE_NOTE/4);
+        playNote(mpCs, WHOLE_NOTE/4);
+        playNote(mpCs, WHOLE_NOTE/4);
+        playNote(mpB, WHOLE_NOTE/2);
+    }
+
+    public void challenge9(View v) {
+        line1();
+        line2();
+        line2();
+        line1();
     }
 }
