e6d32c639167beda6f61119dc2f20d8ce8fcb807
School <SOwyatt@users.noreply.github.com>
2019-02-26 14:10:26 -0500

    2/26/2019

--- CollegeApp214/app/src/main/java/org/pltw/examples/collegeapp214/FamilyMemberFragment.java
+++ CollegeApp214/app/src/main/java/org/pltw/examples/collegeapp214/FamilyMemberFragment.java
@@ -4,8 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class FamilyMemberFragment extends android.support.v4.app.Fragment {
+    
+    private TextView mFirstNameTextView;
+    private TextView mLastNameTestView;
+    private EditText mFirstNameEditText;
+    private EditText mLastNameEditText;
+    
+    private FamilyMember mFamilyMemberModel;
+    
+    
     public View onCreateView(LayoutInflater l, ViewGroup v, Bundle b) {
         super.onCreateView(l, v, b);
         View rootView = l.inflate(R.layout.fragment_family_member, v, false);
